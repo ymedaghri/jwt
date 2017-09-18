60afd6be027a4d792269a58883283b0b3d37f436
Youssef Medaghri-Alaoui <ymedaghri@gmail.com>
2017-09-18 14:48:19 +0200

    Adding GameResource API endpoint

---
+++ src/main/java/com/ymedaghri/retrogames/dto/RetroGame.java
@@ -0,0 +1,15 @@
+package com.ymedaghri.retrogames.dto;
+
+/**
+ * Created by medaghrialaouiyoussef on 18/09/2017.
+ */
+public class RetroGame {
+
+    public String id;
+    public String name;
+
+    public RetroGame(final String id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
